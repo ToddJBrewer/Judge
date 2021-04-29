208ca635938faa0674a108c7943bfa72bbe6737b
Toddjbrewer <Toddjbrewer@gmail.com>
2021-04-29 10:41:49 -0700

    Initial commit

---
+++ src/Judge.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Judge {
+
+    //public int findJudge (int N, int[][] trust) {
+    //}
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{2, 1}, {3, 1}, {4,1}, {2, 3}, {3, 2}};
+        //int[][] arr2 = {{0, 1}, {1, 0}};
+        //int[][] arr3 = {{0, 1}, {1, 0}};
+        //int[][] arr4 = {{0, 1}, {1, 0}};
+        //int[][] arr5 = {{0, 1}, {1, 0}};
+        //int[][] arrNone = {{0, 1}, {1, 0}};
+        //int[][] test = {{0, 1}, {1, 0}};
+
+
+        System.out.println(arr1[1][0]);
+        System.out.println(arr1[1][1]);
+        System.out.println(Arrays.deepToString(arr1));
+
+
+
+    }
+}
